fd24ddd8310e366c176511195214ec9bbc8b2bd6
anunaidu <38093683+anunaidu@users.noreply.github.com>
2021-05-26 23:06:50 +0530

    Create retention.sh

---
+++ retention.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+filepath=".\genlist.txt"
+
+#add starting format of the string
+data="^("
+
+#Loop each line in file and concat the data into variable
+while read line; do
+	#If the line contains empty or space it will neglet
+	if [ ! -z "$line" -a "$line" != " " ]; then
+	   data+=$line"|"	
+	fi
+done < $filepath
+
+#remove last character '|' of the variable
+data=${data%?} #remove last character
+
+#complete the format with ')'
+data+=")"
+
+#write the command line 
+echo "$data"
+
